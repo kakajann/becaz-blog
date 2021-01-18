c8c20ad6e35c9feb0ecc95c8e6cd7ca5a75358f9
Donnie Darko <im@kakajann.com>
2021-01-18 17:32:43 +0500

    blog item types

--- lib/types/blogs.d.ts
+++ lib/types/blogs.d.ts
@@ -1,3 +1,16 @@
+type Author = {
+  fullname: string
+}
+
+type Category = {
+  id: number
+  title: string
+}
+
 type BlogItem = {
   title: string
+  description: string
+  author: Author
+  category: Category
+  publishedAt: string
 }
